a1eb5fbd23e59a076006f85e17e0faf981a77f2b
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-02-22 23:13:50 -0300

    [UPDATE] - Melhoria do thema da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          fontFamily: 'Raleway',
+          textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: Colors.black,
+            displayColor: Colors.blue[600],
+          ),
+          // This colors the [InputOutlineBorder] when it is selected
+          primaryColor: Colors.blue[500],
+          textSelectionHandleColor: Colors.green[500],
         ),
         home: SignIn());
   }
