1d75c0536aa655d22a36d4cf8eaec1c53a840557
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-03-27 21:41:50 -0300

    [UPDATE] - Incluindo um teste unitario para criar e publicar um schedule

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,8 +15,24 @@ void main() {
     final response = await http.get(url);
     expect(response.statusCode, 200);
     final jsonString = response.body;
-    print(jsonString);
     String salvador = "Salvador";
     expect(true, jsonString.contains(salvador));
+  });
+
+  test('Test create schedule ', () async {
+    final url = "http://localhost:8080/schedule?placeID=ChIJrTLr-GyuEmsRBfy61i59si0&numberDays=2";
+    final response = await http.post(url);
+    expect(response.statusCode, 200);
+    final jsonString = response.body;
+    String mockReturn = '{"qtdDays":2,"scheduleDay":null,"imageUrl":"https://s3.amazonaws.com/bk-static-prd-newctn/files/styles/discover_destaque/s3/2016-12/42%20-%20Salvador%20de%20Bahia_4.jpg?itok=2NW2cjVV","nameCity":"ChIJrTLr-GyuEmsRBfy61i59si0","scheduleCod":1,"priceFinal":0}';
+    expect(mockReturn, jsonString);
+  });
+
+  test('Test publish schedule ', () async {
+    final url = "http://localhost:8080/schedule/publish?travelId=1";
+    final response = await http.post(url);
+    expect(response.statusCode, 200);
+    final jsonString = response.body;
+    expect("true", jsonString);
   });
 }
