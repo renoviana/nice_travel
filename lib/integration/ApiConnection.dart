1f23276280fef443656590151cd26b5295804608
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-12-04 11:52:29 -0300

    [UPDATE] - Atualizando a URL da API para utilizar o heroku

--- lib/integration/ApiConnection.dart
+++ lib/integration/ApiConnection.dart
@@ -1,6 +1,6 @@
 class ApiConnection {
-//  static const URL_API = "https://nice-travel2.herokuapp.com";
-  static const URL_API = "http://192.168.25.185:8080";
+  static const URL_API = "https://nice-travel2.herokuapp.com";
+//  static const URL_API = "http://192.168.25.185:8080";
 
   static String encodeFull(String url) {
     var urlFormatted = Uri.encodeFull(url);
